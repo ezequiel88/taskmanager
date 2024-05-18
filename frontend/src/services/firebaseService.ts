900573191aee8dd4cd2b61a578bd3612bd1e6b92
Ezequiel Tavares <eot_ccb@hotmail.com>
2024-05-18 00:05:59 -0300

    Update environment firebase

--- frontend/src/services/firebaseService.ts
+++ frontend/src/services/firebaseService.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app'
 
 const config = {
-  apiKey: 'AIzaSyDM0KNoknrjqONxP75EJ1EACnFC7Ub0l-k',
-  authDomain: 'teste-fcm-2022.firebaseapp.com',
-  databaseURL: 'https://teste-fcm-2022.firebaseio.com',
-  projectId: 'teste-fcm-2022',
-  storageBucket: 'teste-fcm-2022.appspot.com',
-  messagingSenderId: '813480984136',
-  appId: '1:813480984136:web:c3461c9409f41ab342627d'
+  apiKey: 'xxx',
+  authDomain: 'xxx.firebaseapp.com',
+  databaseURL: 'https://xxx.firebaseio.com',
+  projectId: 'xxx',
+  storageBucket: 'xxx.appspot.com',
+  messagingSenderId: 'xxx',
+  appId: 'xxx'
 }
 
 const firebaseApp = initializeApp(config)
